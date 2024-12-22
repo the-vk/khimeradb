a3f91b944c4df290d665ed892de9ff026e48370e
Andrey Maraev <the_vk@thevk.net>
2024-12-22 10:20:10 -0800

    re-engineer MemoryLog into generic Log

--- benches/benches.rs
+++ benches/benches.rs
@@ -1,12 +1,16 @@
+use std::cell::RefCell;
+
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use khimeradb::MemoryLog;
+use khimeradb::Log;
 
 pub fn bench_memory_log_10000_appends(c: &mut Criterion) {
-    let mut log = MemoryLog::new();
-    let entry = [0; 100];
-    
     c.bench_function("MemoryLog 10000 appends", |b| b.iter(|| {
+        let storage:Vec<u8> = Vec::new();
+        let cursor = RefCell::new(std::io::Cursor::new(storage));
+        let mut log = Log::new(cursor);
+        let entry = [0; 100];
+
         for _ in 0..black_box(10000) {
             log.append(&entry);
         }
@@ -14,13 +18,16 @@ pub fn bench_memory_log_10000_appends(c: &mut Criterion) {
 }
 
 pub fn bench_memory_log_10000_iterator(c: &mut Criterion) {
-    let mut log = MemoryLog::new();
-    let entry = [0; 100];
-    for _ in 0..10000 {
-        log.append(&entry);
-    }
+    
     
     c.bench_function("MemoryLog 10000 iterator", |b| b.iter(|| {
+        let storage:Vec<u8> = Vec::new();
+        let cursor = RefCell::new(std::io::Cursor::new(storage));
+        let mut log = Log::new(cursor);
+        let entry = [0; 100];
+        for _ in 0..10000 {
+            log.append(&entry);
+        }
         for _ in log.into_iter() {
         }
     }));
--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,100 @@
-pub struct MemoryLog {
+use std::{cell::RefCell, io::{Read, Seek, SeekFrom, Write}};
+
+pub struct Log<T>
+    where T: Read + Write + Seek {
     // The log entries
-    entries: Vec<Vec<i8>>
+    storage: RefCell<T>,
 }
 
-impl MemoryLog {
+impl <T> Log<T>
+    where T: Read + Write + Seek {
     // Create a new MemoryLog
-    pub fn new() -> MemoryLog {
-        MemoryLog {
-            entries: Vec::new()
+    pub fn new(storage: RefCell<T>) -> Log<T> {
+        Log {
+            storage
         }
     }
 
     // Append a new entry to the log
-    pub fn append(&mut self, entry: &[i8]) {
-        self.entries.push(entry.to_vec());
+    pub fn append(&mut self, entry: &[u8]) -> std::io::Result<()> {
+        let size = entry.len() as u64;
+        let size_bytes = size.to_be_bytes();
+        self.storage.borrow_mut().seek(SeekFrom::End(0))?;
+        self.storage.borrow_mut().write(&size_bytes)?;
+        self.storage.borrow_mut().write(entry)?;
+
+        Ok(())
     }
 }
 
-impl<'a> IntoIterator for &'a MemoryLog {
-    type Item = &'a [i8];
-    type IntoIter = MemoryLogIterator<'a>;
+impl<'a, T> IntoIterator for &'a Log<T>
+    where T: Read + Write + Seek {
+    type Item = Box<[u8]>;
+    type IntoIter = LogIterator<'a, T>;
 
     fn into_iter(self) -> Self::IntoIter {
-        MemoryLogIterator::new(&self.entries)
-    }
-}
-
-pub struct MemoryLogIterator<'a> {
-    // The log entries
-    entries: &'a Vec<Vec<i8>>,
-    // The current index
-    index: usize
-}
-
-impl<'a> MemoryLogIterator<'a> {
-    // Create a new MemoryLogIterator
-    pub fn new(entries: &'a Vec<Vec<i8>>) -> MemoryLogIterator<'a> {
-        MemoryLogIterator {
-            entries: entries,
-            index: 0
+        LogIterator {
+            log: &self.storage,
+            position: 0,
         }
     }
 }
 
-impl<'a> Iterator for MemoryLogIterator<'a> {
-    type Item = &'a [i8];
+pub struct LogIterator<'a, T>
+    where T: Read + Write + Seek {
+    log: &'a RefCell<T>,
+    position: u64,
+}
 
+impl<'a, T> Iterator for LogIterator<'a, T>
+    where T: Read + Write + Seek {
+    type Item = Box<[u8]>;
+    
     fn next(&mut self) -> Option<Self::Item> {
-        if self.index < self.entries.len() {
-            let entry = &self.entries[self.index];
-            self.index += 1;
-            Some(entry)
-        } else {
-            None
+        let mut log = self.log.borrow_mut();
+        if let Err(_) = log.seek(SeekFrom::Start(self.position)) {
+            return None;
         }
+
+        let mut size_bytes = [0; 8];
+        match log.read(&mut size_bytes) {
+            Ok(0) => return None,
+            Err(_) => return None,
+            _ => {}
+        }
+
+        let size = u64::from_be_bytes(size_bytes) as usize;
+        let mut entry = vec![0; size];
+        match log.read(&mut entry) {
+            Ok(0) => return None,
+            Err(_) => return None,
+            _ => {}
+        }
+
+        self.position += 8 + size as u64;
+        Some(entry.into_boxed_slice())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::Log;
+    use std::cell::RefCell;
+
+    #[test]
+    fn test_log() {
+        let count = 100;
+        let storage:Vec<u8> = Vec::new();
+        let cursor = RefCell::new(std::io::Cursor::new(storage));
+        let mut log = Log::new(cursor);
+        let entry = [0; 100];
+        for _ in 0..count {
+            log.append(&entry).unwrap();
+        }
+        let mut count = 0;
+        for _ in log.into_iter() {
+            count += 1;
+        }
+        assert_eq!(count, count);
     }
 }
