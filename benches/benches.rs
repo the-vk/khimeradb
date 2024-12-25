d84bd260222f05345165c585b6d444d3b14d2e6c
Andrey Maraev <the_vk@thevk.net>
2024-12-25 15:16:42 -0800

    update bench sample size to make results more stable

--- benches/benches.rs
+++ benches/benches.rs
@@ -4,26 +4,30 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use khimeradb::{FileSegmentStream, Log};
 use tempfile::tempfile;
 
+const MESSAGE_SIZE: usize = 1024;
+const MEMORY_ITERATIONS: usize = 1000000;
+const FILE_ITERATIONS: usize = 1000;
+
 pub fn bench_memory_log_10000_appends(c: &mut Criterion) {
-    c.bench_function("MemoryLog 10000 appends", |b| b.iter(|| {
+    c.bench_function("MemoryLog appends", |b| b.iter(|| {
         let storage:Vec<u8> = Vec::new();
         let cursor = RefCell::new(std::io::Cursor::new(storage));
         let mut log = Log::new(cursor);
-        let entry = [0; 100];
+        let entry = [0; MESSAGE_SIZE];
 
-        for _ in 0..black_box(100000) {
+        for _ in 0..black_box(MEMORY_ITERATIONS) {
             let _ = log.append(&entry);
         }
     }));
 }
 
 pub fn bench_memory_log_10000_iterator(c: &mut Criterion) {
-    c.bench_function("MemoryLog 10000 iterator", |b| b.iter(|| {
+    c.bench_function("MemoryLog iterator", |b| b.iter(|| {
         let storage:Vec<u8> = Vec::new();
         let cursor = RefCell::new(std::io::Cursor::new(storage));
         let mut log = Log::new(cursor);
-        let entry = [0; 100];
-        for _ in 0..100000 {
+        let entry = [0; MESSAGE_SIZE];
+        for _ in 0..MEMORY_ITERATIONS {
             let _ = log.append(&entry);
         }
         for _ in log.into_iter() {
@@ -32,12 +36,12 @@ pub fn bench_memory_log_10000_iterator(c: &mut Criterion) {
 }
 
 pub fn bench_file_log_10000_iterator(c: &mut Criterion) {
-    c.bench_function("File Log 10000 iterator", |b| b.iter(|| {
+    c.bench_function("File Log iterator", |b| b.iter(|| {
         let file = tempfile().unwrap();
         let file = RefCell::new(file);
         let mut log = Log::new(file);
-        let entry = [0; 100];
-        for _ in 0..10000 {
+        let entry = [0; MESSAGE_SIZE];
+        for _ in 0..FILE_ITERATIONS {
             let _ = log.append(&entry);
         }
         for _ in log.into_iter() {
@@ -46,28 +50,28 @@ pub fn bench_file_log_10000_iterator(c: &mut Criterion) {
 }
 
 pub fn bench_file_segment_log_10000_appends(c: &mut Criterion) {
-    c.bench_function("FileSegmentLog 10000 appends", |b| b.iter(|| {
+    c.bench_function("FileSegmentLog appends", |b| b.iter(|| {
         let tempdir = tempfile::tempdir().unwrap();
         let storage = FileSegmentStream::new(tempdir.path().to_path_buf(), 1024);
         let mut log = Log::new(RefCell::new(storage));
 
-        let data = [0; 1024];
+        let data = [0; MESSAGE_SIZE];
 
-        for _ in 0..black_box(10000) {
+        for _ in 0..black_box(FILE_ITERATIONS) {
             let _ = log.append(&data);
         }
     }));
 }
 
 pub fn bench_file_segment_log_10000_iterator(c: &mut Criterion) {
-    c.bench_function("FileSegmentLog 10000 iterations", |b| b.iter(|| {
+    c.bench_function("FileSegmentLog iterations", |b| b.iter(|| {
         let tempdir = tempfile::tempdir().unwrap();
         let storage = FileSegmentStream::new(tempdir.path().to_path_buf(), 1024);
         let mut log = Log::new(RefCell::new(storage));
 
         let data = [0; 1024];
 
-        for _ in 0..black_box(10000) {
+        for _ in 0..black_box(FILE_ITERATIONS) {
             let _ = log.append(&data);
         }
 
